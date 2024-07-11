88683d7c599265930ca47c51e56aecf8d497e6c1
ali <adadashipour@gmail.com>
2024-07-11 11:57:34 +0100

    Add basic LinkedList classes and function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
+class Node {
+    constructor(value){
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    append(value) {
+        const newNode = new Node(value);
+        if(!this.head){
+            // If the list is empty, the new node is both the head and tail
+            this.head = newNode;
+            this.tail = newNode;
+        }else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+    }
+}
 
